3c3a6f0bb7714edff0c50e5a8d7136bb4c5f1d91
GeovanaCunha <geovanahcunha1109@gmail.com>
2025-04-15 10:18:00 -0300

    f 0.3

---
+++ exercicio 4/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 4</title>
+    <link href="styles.css" rel="stylesheet" >
+</head>
+<body>
+<h1>NÚMERO DA SORTE</h1>
+
+<form action="calcular.php" method="post">
+  <p>Escolha um dos times a seguir:</p>
+  <h2><input type="radio" name="time" required>Palmeiras</h2>
+  <h2><input type="radio" name="time" required>Corinthians</h2>
+  <h2><input type="radio" name="time" required>São Paulo</h2>
+  <h2><input type="radio" name="time" required>Santos</h2>
+  
+  <button type="submit">Enviar</button>
+</form>
+</body>
+</html> 
