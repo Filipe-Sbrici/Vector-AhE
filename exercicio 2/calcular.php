fdbb3b6ee32d651d2c5efa1e0fdbeb9b0a94e931
Caua-Alborghette <cauaalborghette09@gmail.com>
2025-04-15 22:06:28 -0300

    c 0.2

--- exercicio 2/calcular.php
+++ exercicio 2/calcular.php
@@ -3,22 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercício 1</title>
+    <title>Exercício 2</title>
     <link href="styles.css" rel="stylesheet" >
 </head>
 <body>
 <div class = "chickenjockey">
 <h1>NÚMERO ALEATÓRIO</h1>
 <form method="post">
-    <button type="submit" name="sortear">Girar</button>
+    
 </form>
 
 <?php
-$base = rand(0, 99);
+$random = $_POST["num"]
+$base = rand(0, "num");
 
 echo "<h2>Número aleatório: $base</h2>";
 
 ?>
+<button type="submit">Denovo</button>
+</form>
 </div>
 </body>
-</html>  
+</html> 
---
+++ exercicio 2/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 2</title>
+    <link href="styles.css" rel="stylesheet" >
+</head>
+<body>
+<div class = "chickenjockey">
+<h1>NÚMERO ALEATÓRIO</h1>
+<form method="post" action = "calcular.php">
+    
+    <p>Digite um número maior que 0:</p>
+    <input type="number" name="num" min="0" max="100" required>
+
+  <button type="submit">Enviar número</button>
+  </form>
+</div>
+</body>
+</html> 
